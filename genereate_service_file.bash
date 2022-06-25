9a18a14352a1d726618fd9cec3967fdac55039df
simheo <32677536+simheo@users.noreply.github.com>
2022-06-25 17:50:31 +0200

    Add bash files to setup user service.

---
+++ genereate_service_file.bash
@@ -0,0 +1,14 @@
+# Generate reachy_mobile_base.service
+# Put this service file in ~/.config/systemd/user
+
+tee reachy_mobile_base.service <<EOF
+[Unit]
+Description=Mobile base SDK server service
+
+[Service]
+ExecStart=/usr/bin/bash /home/reachy/reachy_ws/src/mobile_base_sdk_server/launch_mobile_base.bash
+Environment="PATH=/opt/ros/foxy/bin:/home/reachy/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/reachy/.local>
+
+[Install]
+WantedBy=default.target
+EOF
---
+++ launch_mobile_base.bash
@@ -0,0 +1,15 @@
+# source ROS2 Foxy setup file.
+# shellcheck disable=SC1091
+source /opt/ros/foxy/setup.bash
+source $HOME/reachy_ws/install/setup.bash
+
+# Start the ROS2 launch file
+zuuu_model=$(reachy-identify-zuuu-model)
+
+if [ $zuuu_model != none ];
+then
+    echo "Starting mobile base launch file."
+    ros2 launch mobile_base_sdk_server run_mobile_base_sdk_server_and_hal.launch.py
+else
+    echo "No mobile base found in this Reachy model."
+fi
