7a6c12f0b81e922cef6a4a0ba30755991ee16014
glannuzel <glannuzel@ensc.fr>
2022-06-29 14:46:06 +0200

    Change launch file.

--- launch/run_mobile_base_sdk_server_and_hal.launch.py
+++ launch/run_mobile_base_sdk_server_and_hal.launch.py
@@ -1,32 +1,31 @@
-from launch import LaunchDescription
-
-from launch_ros.actions import Node
-
 import os
 from ament_index_python.packages import get_package_share_directory
 
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
 
 def generate_launch_description():
-    ld = LaunchDescription()
 
-    config = os.path.join(
-        get_package_share_directory('zuuu_hal'),
-        'config',
-        'params.yaml'
-    )
+    zuuu_hal_dir = get_package_share_directory('zuuu_hal')
 
-    ld.add_action(
-       Node(
-        package='zuuu_hal',
-        executable='hal',
-        parameters=[config]
+    arguments = []
+
+    launches = [
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(zuuu_hal_dir, 'hal.launch.py')
+            )
         )
-    )
+    ]
 
-    ld.add_action(
+    nodes = [
         Node(
             package='mobile_base_sdk_server',
             executable='mobile_base_sdk_server',
         ),
-    )
-    return ld
+    ]
+
+    return LaunchDescription(arguments + launches + nodes)
