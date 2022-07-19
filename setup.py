d5470cd50b939f5f51b9f5d02dcfd448cdbb395d
simheo <simheo0@gmail.com>
2022-07-19 15:30:30 +0200

    Add dependencies in setup.py.

--- setup.py
+++ setup.py
@@ -15,7 +15,10 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('launch/*.py')),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools',
+        'reachy_sdk_api>0.6.0',
+        ],
     zip_safe=True,
     maintainer='reachy',
     maintainer_email='32677536+simheo@users.noreply.github.com',
