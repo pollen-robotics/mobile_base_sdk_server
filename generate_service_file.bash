7e923e799f47f8a105d5e2f40d4951238e844a56
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2022-08-02 18:42:18 +0200

    Add sleep in service file.

--- generate_service_file.bash
+++ generate_service_file.bash
@@ -6,6 +6,7 @@ tee reachy_mobile_base.service <<EOF
 Description=Mobile base SDK server service
 
 [Service]
+ExecStartPre=/bin/sleep 10
 ExecStart=/usr/bin/bash /home/reachy/reachy_ws/src/mobile_base_sdk_server/launch_mobile_base.bash
 Environment="PATH=/opt/ros/foxy/bin:/home/reachy/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/reachy/.local>
 
