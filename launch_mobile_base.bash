bd4f2dacdfb47c4f63465157704b77ab1608c9d1
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2022-08-02 18:49:10 +0200

    Update launch_mobile_base.bash

--- launch_mobile_base.bash
+++ launch_mobile_base.bash
@@ -1,8 +1,13 @@
+#!/bin/bash
 # source ROS2 Foxy setup file.
 # shellcheck disable=SC1091
 source /opt/ros/foxy/setup.bash
 source $HOME/reachy_ws/install/setup.bash
 source $HOME/reachy_tips/config/reachy_ros_config
+source $HOME/.bashrc
+
+IDENTIFY_CMD=$HOME/.local/bin/reachy-identify-zuuu-model
+zuuu_model=`$IDENTIFY_CMD`
 
 # Start the ROS2 launch file
 zuuu_model=$(reachy-identify-zuuu-model)
