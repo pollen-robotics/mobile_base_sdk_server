a235ee405e89f2d8bb1168cc589f61d78a01c298
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2022-08-04 17:48:57 +0200

    Update generate_service_file.bash

--- generate_service_file.bash
+++ generate_service_file.bash
@@ -7,8 +7,8 @@ Description=Mobile base SDK server service
 
 [Service]
 ExecStartPre=/bin/sleep 10
-ExecStart=/usr/bin/bash /home/reachy/reachy_ws/src/mobile_base_sdk_server/launch_mobile_base.bash
-Environment="PATH=/opt/ros/foxy/bin:/home/reachy/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/reachy/.local>
+ExecStart=/usr/bin/bash $PWD/launch_mobile_base.bash
+Environment="PATH=$PATH:$(dirname $(which reachy-identify-model))"
 
 [Install]
 WantedBy=default.target
